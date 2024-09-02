809410658dcfc6948e4e828666975e5a7e281582
Pranav P Ninawe <97885764+PranavN07@users.noreply.github.com>
2024-09-03 02:26:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (8.45%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        for(char c:s){
+            if(c=='('||c=='{'||c=='['){
+                st.push(c);
+
+            }
+            else{
+                if(st.empty()||
+                (c==')' && st.top() != '(' )||
+                (c=='}' && st.top() != '{' )||
+                (c==']' && st.top() != '['))
+                {
+                    return false;
+                }
+                else{
+                    st.pop();
+                }
+            }
+
+        }
+        return st.empty();
+        
+        
+    }
+};
