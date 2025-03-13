b3dc68557ad950e109f34b46a55e08feffe0fcaa
Pranav P Ninawe <97885764+PranavN07@users.noreply.github.com>
2025-03-13 10:22:02 +0530

    Time: 4 ms (16.32%) | Memory: 21.7 MB (79.55%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,28 @@
+
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> res;
+
+        for (int a : asteroids) {
+            while (!res.empty() && a < 0 && res.back() > 0) {
+                if (abs(a) > res.back()) {
+                    res.pop_back();
+                    continue;
+                } else if (abs(a) == res.back()) {
+                    res.pop_back();
+                }
+                a = 0;  // Both explode
+            }
+            if (a != 0) {
+                res.push_back(a);
+            }
+        }
+
+        return res;
+    }
+};
