df53d31fe28e95f3e1d722db5a97998b392571e1
Pranav P Ninawe <97885764+PranavN07@users.noreply.github.com>
2025-05-13 21:31:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (65.43%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -3,8 +3,8 @@ public:
     int subsetXORSum(vector<int>& nums) {
         int total = 0;
         for (int num : nums) {
-            total |= num;  // Step 1: Compute bitwise OR of all numbers
+            total |= num; 
         }
-        return total * (1 << (nums.size() - 1));  // Step 2: Multiply by 2^(n-1)
+        return total * (1 << (nums.size() - 1));
     }
 };
